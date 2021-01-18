1bb7b64845e30872ad4effa5e3e25b17d9870ff9
Mustafa-ilahi <mustafailahi586@gmail.com>
2021-01-18 22:26:27 +0500

    practice

--- firstreactproject/src/App.js
+++ firstreactproject/src/App.js
@@ -12,14 +12,23 @@ class App extends React.Component{
   constructor(){
     super()
     this.state = {
-      name: "Mustafa",
-      email: "mustafa@gmail.com"
+      name: "Hello World",
+      // email: "mustafa@gmail.com"
     }
   }
+  changeName = () =>{
+    // console.log("Hello")
+    this.setState({
+      name : "Hello Pakistan"
+    })
+  }
+
   render(){
     return(
       <div>
-        <h1>{`Welcome ` + this.state.name + `..!`}</h1>
+        <h1>{this.state.name}</h1>
+        <button  onClick={this.changeName}>Change Name</button>
+        {/* <h2>{`Email: ` + this.state.email}</h2> */}
           {/* <Header />
           <h1 className="mainComponent">Main Component</h1>
           <Footer />
